22b194c63759f5f2f64d703c7d788822088cc7cb
nonrita <2220025@ecc.ac.jp>
2024-05-21 15:53:19 +0900

    add:Headerを追加しました

---
+++ app/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script></script>
+
+<template>
+  <header>
+    <h1>Todo</h1>
+  </header>
+</template>
+
+<style>
+header {
+  outline: 1px solid plum;
+  background-color: #000;
+  padding-left: 1em;
+  h1 {
+    line-height: 1.5em;
+    height: 1.5em;
+    color: #fff;
+  }
+}
+</style>
