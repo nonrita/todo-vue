2b0b29e68797f81b0e9a9ad67a68be229cbf605d
nonrita <2220025@ecc.ac.jp>
2024-05-24 11:14:38 +0900

    add:todo-listを追加しました

--- app/src/components/MainArea.vue
+++ app/src/components/MainArea.vue
@@ -13,6 +13,38 @@
       />
       <button class="add-button"><fa icon="add" /></button>
     </form>
+
+    <ul class="todo-list">
+      <li class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" name="i" id="">
+          <h1>Reactの勉強</h1>
+        </div>
+        <div class="todo-item-right">
+          <button><fa class="icon" icon="trash" /></button>
+        </div>
+      </li>
+
+      <li class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" name="i" id="">
+          <h1>Reactの勉強</h1>
+        </div>
+        <div class="todo-item-right">
+          <button><fa class="icon" icon="trash" /></button>
+        </div>
+      </li>
+
+      <li class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" name="i" id="">
+          <h1>Reactの勉強</h1>
+        </div>
+        <div class="todo-item-right">
+          <button><fa class="icon" icon="trash" /></button>
+        </div>
+      </li>
+    </ul>
   </main>
 </template>
 
@@ -21,7 +53,8 @@ main {
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  padding: 24px 20% 0 20%;
+  padding: 24px 25% 0 25%;
+  gap: 24px;
   .todo-input-area {
     display: flex;
     justify-content: center;
@@ -38,6 +71,40 @@ main {
       border-radius: 20px;
       height: 40px;
       aspect-ratio: 1 / 1;
+    }
+  }
+  .todo-list {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    gap: 24px;
+    .todo-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: end;
+      padding: 0 16px 8px 8px;
+      border-bottom: 1px solid #000 ;
+      .todo-item-left {
+        display: flex;
+        gap: 8px;
+        height: fit-content;
+        h1 {
+          font-size: 1.5rem;
+        }
+      }
+      .todo-item-right {
+        button {
+          background-color: transparent;
+          border: transparent;
+          height: fit-content;
+          padding: 0;
+          .icon {
+            height: 24px;
+          }
+        }
+      }
     }
   }
 }
