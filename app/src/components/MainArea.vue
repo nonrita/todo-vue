ff74cb4c93b89c0264cc320d7c3f78538a0dc430
nonrita <2220025@ecc.ac.jp>
2024-05-24 10:04:32 +0900

    add:mainにtodoを追加するためのフォームを作成しました

--- app/src/components/MainArea.vue
+++ app/src/components/MainArea.vue
@@ -1,15 +1,45 @@
-<script>
-</script>
+<script></script>
 
 <template>
   <main>
-    main
+    <form class="todo-input-area">
+      <input
+        class="todo-input"
+        type="text"
+        size="32"
+        maxlength="32"
+        required
+        placeholder="Please enter new Todo"
+      />
+      <button class="add-button"><fa icon="add" /></button>
+    </form>
   </main>
 </template>
 
 <style>
 main {
   flex-grow: 1;
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px 20% 0 20%;
+  .todo-input-area {
+    width: fit-content;
+    display: flex;
+    gap: 16px;
+    .todo-input {
+      height: fit-content;
+      padding: 0.5em 1em;
+      border: 1px solid #afafaf;
+      border-radius: 2.5em;
+    }
+    .add-button {
+      background-color: #000;
+      color: #fff;
+      border-radius: 20px;
+      height: 40px;
+      aspect-ratio: 1 / 1;
+    }
+  }
 }
 </style>
