930cfbc5743fb127b155c2b2389436370714ee8c
nonrita <2220025@ecc.ac.jp>
2024-05-24 11:14:01 +0900

    add:使うアイコンを追加しました

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAdd } from '@fortawesome/free-solid-svg-icons'
+import { faAdd, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faAdd)
+library.add(faAdd, faTrash)
 
 var app = createApp(App)
 app.component('fa', FontAwesomeIcon )
